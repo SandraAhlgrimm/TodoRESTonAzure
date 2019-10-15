e7b1f1e789b0e4b4f205ad389fcde206c7b4fe03
Sandra Ahlgrimm <sandra.kriemann@googlemail.com>
2019-10-15 11:53:40 +0200

    override with working solution

---
+++ Droid/HttpClientHandlerService.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http;
+using Android.Net;
+using Javax.Net.Ssl;
+using TodoREST.Droid;
+using Xamarin.Android.Net;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(HttpClientHandlerService))]
+namespace TodoREST.Droid
+{
+    public class HttpClientHandlerService : IHttpClientHandlerService
+    {
+        public HttpClientHandler GetInsecureHandler()
+        {
+            return new IgnoreSSLLocalHostClientHandler();
+        }
+
+        internal class IgnoreSSLLocalHostClientHandler : AndroidClientHandler
+        {
+            protected override SSLSocketFactory ConfigureCustomSSLSocketFactory(HttpsURLConnection connection)
+            {
+                return SSLCertificateSocketFactory.GetInsecure(1000, null);
+            }
+
+            protected override IHostnameVerifier GetSSLHostnameVerifier(HttpsURLConnection connection)
+            {
+                return new IgnoreSSLLocalhostHostnameVerifier();
+            }
+        }
+
+        internal class IgnoreSSLLocalhostHostnameVerifier : Java.Lang.Object, IHostnameVerifier
+        {
+            public bool Verify(string hostname, ISSLSession session)
+            {
+                if (hostname.Equals("10.0.2.2"))
+                    return true;
+                return false;
+            }
+        }
+    }
+}
---
+++ Droid/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace TodoREST.Droid
+{
+    [Activity(Label = "TodoREST.Droid", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        internal static MainActivity Instance { get; private set; }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+            Instance = this;
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            #if DEBUG
+                    System.Net.ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+            #endif
+
+            LoadApplication(new App());
+        }
+    }
+}
+
